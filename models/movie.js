90c4149bb206307fa56d43d38d32ed449143c392
HuseinHQ <mhusein463@gmail.com>
2023-10-02 16:04:16 +0700

    Database initialization

---
+++ models/movie.js
@@ -0,0 +1,60 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Movie.belongsTo(models.Genre);
+      Movie.belongsTo(models.User);
+    }
+  }
+  Movie.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Title is required'
+        },
+        notEmpty: {
+          msg: 'Title is required'
+        }
+      }
+    },
+    synopsis: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Synopsis is required'
+        },
+        notEmpty: {
+          msg: 'Synopsis is required'
+        }
+      }
+    },
+    trailerUrl: DataTypes.STRING,
+    imgUrl: DataTypes.STRING,
+    rating: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: 1,
+          msg: 'Minimal rating is 1'
+        }
+      }
+    },
+    genreId: DataTypes.INTEGER,
+    authorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Movie',
+  });
+  return Movie;
+};
---
+++ models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Movie)
+    }
+  }
+  User.init({
+    username: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          msg: 'Email is required'
+        },
+        isEmail: {
+          msg: 'Type must be Email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          msg: 'Password is required'
+        },
+        len: {
+          args: [5, Infinity],
+          msg: 'Minimal length of password is 5'
+        }
+      }
+    },
+    role: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    address: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
