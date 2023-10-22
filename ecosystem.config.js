8977af5aa24110288be0a787f84958f98f6ce29f
HuseinHQ <mhusein463@gmail.com>
2023-10-22 19:37:33 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
         DATABASE_URL: "postgresql://postgres:jhhntuZYiNmi0wui@db.hpcayumwifnnltrpqpyq.supabase.co:5432/postgres",
         SECRET_KEY: "ThisIsMyAppSecretKey@463128",
         GOOGLE_CLIENT_ID: "782243135980-7dfmhrfqah531g5ob5u200trjrkl0ah2.apps.googleusercontent.com",
+        QR_CODE_API_KEY: "lovJjoxgIdbpppOQgovfg_dP5QKpsyplJ-tuE7EkBS-PttiVzrNxWjbmFyizQmmJ",
       },
     },
   ],
 };
-
